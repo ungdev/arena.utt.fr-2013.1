3d88876a9c3eb8f686219f9df3ac5e88fa817c46
uZer <piolet.y@gmail.com>
2013-09-19 18:50:45 +0200

    Upgrade partenaires

--- vues/contenu_corps_partenaires.php
+++ vues/contenu_corps_partenaires.php
@@ -1,19 +1,82 @@
-		<div class="container">
-			<div class="page-header">
-				<h1>Nos partenaires</h1>
-			</div>
-			
-			<h2>Micromania</h2>
-			<p>
-				<center><a href="http://www.micromania.fr/"><img src="images/partners/micromania.jpg" class="img-polaroid" alt="Logo" /></a></center><br />
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-			</p>
-			
-			<h2>Steel Series</h2>
-			<p>
-				<center><a href="http://steelseries.com/"><img src="images/partners/steelseries.jpg" class="img-polaroid" alt="Logo" /></a></center><br />
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-			</p>
-		</div>
+<div class="container">
+    <div class="page-header">
+        <h1>Nos partenaires</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+    <h2>Micromania</h2>
+
+    <p>
+        <center>
+        <a href="http://www.micromania.fr/">
+            <img src="images/partners/micromania.jpg" class="img-responsive" alt="Logo" />
+        </a>
+        </center>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+
+        </div>
+
+        <div class="col-md-4">
+    <h2>More...</h2>
+    <p>
+        <center>
+        <a href="http://steelseries.com/">
+            <img src="images/partners/steelseries.jpg" class="img-responsive" alt="Logo" />
+        </a>
+        </center>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+        </div>
+        <div class="col-md-4">
+    <h2>More...</h2>
+    <p>
+        <center>
+        <a href="http://steelseries.com/">
+            <img src="images/partners/steelseries.jpg" class="img-responsive" alt="Logo" />
+        </a>
+        </center>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+        </div>
+        </div>
+    <div class="row">
+        <div class="col-md-4">
+    <h2>Micromania</h2>
+
+    <p>
+        <center>
+        <a href="http://www.micromania.fr/">
+            <img src="images/partners/micromania.jpg" class="img-responsive" alt="Logo" />
+        </a>
+        </center>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+
+        </div>
+
+        <div class="col-md-4">
+    <h2>More...</h2>
+    <p>
+        <center>
+        <a href="http://steelseries.com/">
+            <img src="images/partners/steelseries.jpg" class="img-responsive" alt="Logo" />
+        </a>
+        </center>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+        </div>
+        <div class="col-md-4">
+    <h2>More...</h2>
+    <p>
+        <center>
+        <a href="http://steelseries.com/">
+            <img src="images/partners/steelseries.jpg" class="img-responsive" alt="Logo" />
+        </a>
+        </center>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </p>
+        </div>
+        </div>
+</div>
