e1597fc6efc2f08c403bbae0209750611db2e20f
bj0rge <bastien.jorge@gmail.com>
2013-09-20 19:33:45 +0200

    Ajout superbe compte à rebours (version bureau)

--- vues/contenu_corps_accueil.php
+++ vues/contenu_corps_accueil.php
@@ -34,95 +34,132 @@
 		
 		<div class="col-lg-6">
 			<p>
-				<div id="compte_a_rebours">
-					<noscript>Fin de l'évènement le 1er janvier 2013.</noscript>	
-					<script type="text/javascript">
-						function compte_a_rebours(){
-							var compte_a_rebours = document.getElementById("compte_a_rebours");
-
-							var date_actuelle = new Date();
-							var date_evenement = new Date("Oct 1 12:00:00 2013");
-							var total_secondes = (date_evenement - date_actuelle) / 1000;
-
-							var prefixe = "Les inscriptions seront lançées dans :<br />";
-							if (total_secondes < 0)
-							{
-								prefixe = "Les inscriptions ont commençé.";
-
-							}
-
-							if (total_secondes > 0)
-							{
-								var jours = Math.floor(total_secondes / (60 * 60 * 24));
-								var heures = Math.floor((total_secondes - (jours * 60 * 60 * 24)) / (60 * 60));
-								minutes = Math.floor((total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
-								secondes = Math.floor(total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
-
-								var et = ", et";
-								var mot_jour = "jours,";
-								var mot_heure = "heures,";
-								var mot_minute = "minutes";
-								var mot_seconde = "secondes";
-
-								if (jours == 0)
-								{
-									jours = '';
-									mot_jour = '';
-								}
-								else if (jours == 1)
-								{
-									mot_jour = "jour,";
-								}
-
-								if (heures == 0)
-								{
-									heures = '';
-									mot_heure = '';
-								}
-								else if (heures == 1)
-								{
-									mot_heure = "heure,";
-								}
-
-								if (minutes == 0)
-								{
-									minutes = '';
-									mot_minute = '';
-								}
-								else if (minutes == 1)
-								{
-									mot_minute = "minute";
-								}
-
-								if (secondes == 0)
-								{
-									secondes = '';
-									mot_seconde = '';
-									et = '';
-								}
-								else if (secondes == 1)
-								{
-									mot_seconde = "seconde";
-								}
-
-								if (minutes == 0 && heures == 0 && jours == 0)
-								{
-									et = "";
-								}
-
-								compte_a_rebours.innerHTML = prefixe + jours + ' ' + mot_jour + ' ' + heures + ' ' + mot_heure + ' ' + minutes + ' ' + mot_minute + et + ' ' + secondes + ' ' + mot_seconde;
-							}
-							else
-							{
-								compte_a_rebours.innerHTML = 'Les inscriptions ont commençé.';
-							}
-
-							var actualisation = setTimeout("compte_a_rebours();", 1000);
-						}
-
-						compte_a_rebours();
-						</script>
+				<?php
+					// On déclare le compteur, et on ajoute 12*60*60 pour rajouter 12h
+					// Si besoin de modifier la date ne pas oublier de le faire également dans countdown.js
+					$fincompteur = (strtotime('October 1, 2013') + (12*60*60));
+					$today = time();
+					
+					// Si on est avant le lancement
+					if (($today - $fincompteur) <= 0)
+					{
+						{ ?>
+						
+				<div id="mycountdown">
+					<p>
+						Les inscriptions seront lancées dans :
+					</p>
+					<link href="css/countdown.css" rel="stylesheet" media="screen">
+					<div id="countdown-blog" style=''></div>
+					<script type="text/javascript" src="js/countdown.js"></script>
 				</div>
+				
+				
+				
+				<div id="compte_a_rebours">
+
+<noscript>Fin de l'évènement le 1er janvier 2013.</noscript>
+<script type="text/javascript">
+function compte_a_rebours()
+{
+    var compte_a_rebours = document.getElementById("compte_a_rebours");
+
+    var date_actuelle = new Date();
+    var date_evenement = new Date("Oct 1 12:00:00 2013");
+    var total_secondes = (date_evenement - date_actuelle) / 1000;
+
+    var prefixe = "Les inscriptions seront lançées dans ";
+    if (total_secondes < 0)
+    {
+        prefixe = "Les inscriptions ont commençé.";
+
+    }
+
+    if (total_secondes > 0)
+    {
+        var jours = Math.floor(total_secondes / (60 * 60 * 24));
+        var heures = Math.floor((total_secondes - (jours * 60 * 60 * 24)) / (60 * 60));
+        minutes = Math.floor((total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
+        secondes = Math.floor(total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
+
+        var et = "et";
+        var mot_jour = "jours,";
+        var mot_heure = "heures,";
+        var mot_minute = "minutes,";
+        var mot_seconde = "secondes";
+
+        if (jours == 0)
+        {
+            jours = '';
+            mot_jour = '';
+        }
+        else if (jours == 1)
+        {
+            mot_jour = "jour,";
+        }
+
+        if (heures == 0)
+        {
+            heures = '';
+            mot_heure = '';
+        }
+        else if (heures == 1)
+        {
+            mot_heure = "heure,";
+        }
+
+        if (minutes == 0)
+        {
+            minutes = '';
+            mot_minute = '';
+        }
+        else if (minutes == 1)
+        {
+            mot_minute = "minute,";
+        }
+
+        if (secondes == 0)
+        {
+            secondes = '';
+            mot_seconde = '';
+            et = '';
+        }
+        else if (secondes == 1)
+        {
+            mot_seconde = "seconde";
+        }
+
+        if (minutes == 0 && heures == 0 && jours == 0)
+        {
+            et = "";
+        }
+
+        compte_a_rebours.innerHTML = prefixe + jours + ' ' + mot_jour + ' ' + heures + ' ' + mot_heure + ' ' + minutes + ' ' + mot_minute + ' ' + et + ' ' + secondes + ' ' + mot_seconde;
+    }
+    else
+    {
+        compte_a_rebours.innerHTML = 'Les inscriptions ont commençé.';
+    }
+
+    var actualisation = setTimeout("compte_a_rebours();", 1000);
+}
+compte_a_rebours();
+</script>
+
+
+
+    </div>
+				
+				
+				
+				
+						<?php }
+					}
+					else
+					{
+						echo "Les inscriptions ont commencé.";
+					}
+				?>
 			</p>
 	
 		
