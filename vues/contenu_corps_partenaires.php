f31ac113f09aceba5ed8485a0fdd720712f41885
uZer <piolet.y@gmail.com>
2013-09-25 01:54:40 +0200

    Upgrade page partenaires

--- vues/contenu_corps_partenaires.php
+++ vues/contenu_corps_partenaires.php
@@ -7,20 +7,22 @@
         </h1>
     </div>
 
+    <div class="panel panel-default">
+        <div class="panel-body">La liste de nos sponsors peut changer en fonction de nos futurs accords de partenariats.</div>
+    </div>
     <div class="row">
         <div class="col-md-4">
-			<h2>BDE</h2>
-			<p>
-				<center>
-					<a href="http://www.bde.utt.fr/"><img src="images/partners/bde.jpg" class="img-responsive" alt="Logo" /></a>
-				</center>
-				Le BDE de l'Université de Technologie de Troyes fédère les associations de l'UTT et facilite l'organisation de leurs projets. Les subventions attribuées par le BDE permettent de couvrir nottamment les frais de fonctionnement des associations.
-			</p>
+			<div class="thumbnail">
+				<img src="images/partners/bde.jpg" class="img-responsive" alt="Logo BDE">
+				<div class="caption">
+				<h3>BDE</h3>
+				<p>Le BDE de l'Université de Technologie de Troyes fédère les associations de l'UTT et facilite l'organisation de leurs projets. Les subventions attribuées par le BDE permettent de couvrir nottamment les frais de fonctionnement des associations.
+				</p>
+				<p><a href="http://bde.utt.fr/" class="btn btn-primary">Site Officiel</a></p>
+				</div>
+			</div>
 		</div>
-	</div>
+    </div>
 
-	<div class="panel panel-default">
-		<div class="panel-body">La liste de nos sponsors peut changer en fonction de nos futurs accords de partenariats.</div>
-	</div>
 
 </div>
