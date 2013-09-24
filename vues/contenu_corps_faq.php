c7838f78e2291fa0793e1db6e2157a7e1f2d677c
Dugong <julien.nozais@gmail.com>
2013-09-24 23:30:34 +0200

    Actualisation de la FAQ pour UA13

--- vues/contenu_corps_faq.php
+++ vues/contenu_corps_faq.php
@@ -1,68 +1,68 @@
-﻿
-<div class="container">
+﻿<div class="container">
 	<div class="page-header">
 		<h1>Questions fréquentes</h1>
 	</div>
 	
 	<div class="list-group">
-		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewcontacts">
+		
+		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewsubscribe">
 			<h3 class="list-group-item-heading">
-				<span class="glyphicon glyphicon-envelope"></span>	Nous contacter 
+				<span class="glyphicon glyphicon-log-in"></span> Inscription à l'UTT Arena 
 			</h3>
-			<div class="list-group-item-text collapse" id="viewcontacts">
-				<h4>
-					Je n'ai pas la réponse à ma question, qui dois-je contacter ?
-				<br />
-				<small>
-					Vous pouvez contacter le staff de l'UA via ung@utt.fr. N'hésitez pas à vous exprimer en cas de doutes ou de remarques.
-				</small>
-				</h4>
+			<div class="list-group-item-text collapse" id="viewsubscribe">
+				<h4>Comment et quand payer ?<br /><small>Deux solutions s'offrent à vous : payer sur le site en prévente, dès l'ouverture des inscriptions, ou payer sur place, plein tarif (espèces uniquement). Les préventes commencent mi-octobre et se terminent 48 h avant l'UA. </small></h4>
+				<h4>Puis-je payer sur place ?<br /><small>Oui, mais une majoration sera appliquée, les prix passeront à 10 € pour les UTTiens, et à 15 € pour les extérieurs.</small></h4>
+				<h4>Puis-je me faire rembourser ? <br /><small>Nous traitons ce genre de demande au cas par cas, avec comme solution donner votre place à votre remplaçant ou trouver un moyen de vous dédommager. Pensez à contacter le staff UA par e-mail.</small></h4>
+				<h4>Les visiteurs sont-ils admis ?<br /><small>Les visiteurs doivent payer un droit d'entrée de 2 € à l'accueil, il leur sera remis un badge qu'ils doivent porter de façon visible. Les visiteurs ayant quitté la sale doivent présenter leur badge à l'accueil s'ils veulent rentrer.</small></h4>
 			</div>
 		</a>
 		
 		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewlan">
 			<h3 class="list-group-item-heading">
-				<span class="glyphicon glyphicon-adjust"></span>	Pendant la LAN
+				<span class="glyphicon glyphicon-adjust"></span> Pendant la LAN
 			</h3>
 			<div class="list-group-item-text collapse" id="viewlan">
-				<h4>A quelle heure commence la LAN ?<br /><small>L'UTT Arena ouvre ses portes le vendredi à 17h. Les animations commencent à xxx. Un planning des évènements, avec des horaires plus précis, vous sera fourni dans la semaine précédant la LAN. </small></h4>
-				<h4>Où puis-je dormir ?<br /><small>Nous pouvons vous fournir une salle de repos pour des pauses, néanmoins nous ne pouvons vous fournir un véritable logement pour la nuit. Nous vous conseillons de trouver une solution, que ce soit de dormir dans un hôtel, de dormir dans vos voitures ou tout simplement de sommeiller devant votre écran.</small></h4>
-				<h4>Comment se sustenter au cours de l'événement ?<br /><small>La commission bouffe (dite aussi commission mangeaille) installera un stand près de l'accueil, avec une équipe chaleureuse et serviable à votre disposition (plus de 20h par jour) pour vous servir en boissons, barres, sandwiches et nourritures diverses. </small></h4>
-				<h4>Quel matériel est fourni/loué ?<br /><small>A part cas exceptionnel, rien n'est fourni. Néanmoins nous louons des écrans et vendons des câbles réseaux RJ-45 de 5 mètres. Les prix seront établis dans les semaines à venir.</small></h4>
-				<h4>Que faire une fois sur place ? <br /><small>Si vous arrivez par train et avez demandé une navette pour vous rendre à la LAN (demande via le forum), nous vous attendrons sur le quai (un numéro de téléphone vous sera fourni pour nous contacter en cas de soucis). Si vous arrivez sur le lieu de l'UA même (halle sportive de l'UTT), rendez vous à l'accueil où vous serez accueillis et obtiendrez votre badge.</small></h4>
-			</div>
-		</a>
-		
-		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewteam">
-			<h3 class="list-group-item-heading">
-				<span class="glyphicon glyphicon-fire"></span>	Les équipes 
-			</h3>
-			<div class="list-group-item-text collapse" id="viewteam">
-				<h4>Comment créer une team ?<br /><small>Dans la liste des participants (accessible aussi via "Ma team" si vous n'êtes dans aucune team), il existe un bouton "Créer une équipe". Remplissez ensuite les champs demandés (URL et logo étant facultatifs). </small></h4>
-				<h4>Comment rejoindre une team ?<br /><small>En se rendant sur la liste des participants (ou via le lien "Ma team") puis sur la page de votre team. Vous pouvez candidater au statut de membre et le chef d'équipe validera, ou non, votre candidature. </small></h4>
-				<h4>Puis-je quitter une team ? <br /><small> Une fois inscrit dans une team, vous disposez toujours d'une option pour la quitter. Vous pouvez accéder à la page de votre team via "Ma team" ou via la liste des participants.</small></h4>
+				<h4>A quelle heure commence la LAN ?<br /><small>L'UTT Arena ouvre ses portes le vendredi à 18 h. Les animations commencent à 19 h. Un planning des animations sera fourni avant la semaine de la LAN.</small></h4>
+				<h4>Où puis-je dormir ?<br /><small>Nous pouvons vous mettre à disposition une salle de repos commune de taille limitée, néanmoins nous ne pouvons vous fournir un véritable logement pour la nuit. Nous vous conseillons de trouver une solution, que ce soit de dormir dans un hôtel, dans vos voitures ou tout simplement de sommeiller devant votre écran.</small></h4>
+				<h4>Où manger au cours de l'événement ?<br /><small>La commission bouffe (dite aussi commission mangeaille) installera un stand près de l'accueil, avec une équipe chaleureuse et serviable à votre disposition (plus de 20h par jour) pour vous servir en boissons, barres, sandwiches, crêpes et nourritures diverses. </small></h4>
+				<h4>Quel matériel est fourni/loué ?<br /><small>A part cas exceptionnel, rien n'est fourni. Vous disposerez d'une prise par personne et d'un port réseau. Nous pouvons louer des écrans cathodiques et vendons des câbles réseaux Ethernet de 5 mètres.</small></h4>
+				<h4>Que faire lorsque j'arrive sur place ? <br /><small>Si vous arrivez par train et avez demandé une navette pour vous rendre à la LAN (demande via le forum), nous vous attendrons sur le quai (un numéro de téléphone vous sera fourni pour nous contacter en cas de soucis). Si vous arrivez sur le lieu de l'UA même (halle sportive de l'UTT), rendez vous à l'accueil où vous serez accueillis et obtiendrez votre badge.</small></h4>
 			</div>
 		</a>
 		
 		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewtournaments">
 			<h3 class="list-group-item-heading">
-				<span class="glyphicon glyphicon-tower"></span>	Les tournois 
+				<span class="glyphicon glyphicon-tower"></span> Les tournois spotlight
 			</h3>
 			<div class="list-group-item-text collapse" id="viewtournaments">
-				<h4>Comment et quand payer ?<br /><small>Deux solutions s'offrent à vous : payer sur le site en prévente, via paypal, ou payer sur place, plein tarif (espèces uniquement). Les préventes commencent début avril et se terminent quelques jours avant l'UA (les dates précises vous seront communiquées sous peu). </small></h4>
-				<h4>Puis-je payer sur place ?<br /><small>Oui. Néanmoins une majoration sera appliquée, les prix passeront à 15€ pour les UTTiens, et à 20€ pour les extérieurs.</small></h4>
-				<h4>Où se trouve le règlement des jeux ? <br /><small>Vous trouverez le règlement de chaque tournoi dans leurs pages respectives ainsi qu'un rappel sur les différents forums.</small></h4>
-				<h4>Puis-je participer à plusieurs tournois en même temps ?<br /><small>Hélas non, il ne vous est pas possible de participer à deux tournois en même temps. Nous en sommes sincèrement désolés.</small></h4>
-				<h4>Puis-je me faire rembourser ? <br /><small>Nous traitons ce genre de demande au cas par cas, avec comme solution donner votre place à votre remplaçant ou trouver un moyen de vous dédommager. Pensez à contacter le staff UA, que ce soit par le forum ou par e-mail.</small></h4>
+				<h4>Qu'est ce qu'un tournois spotlight ?<br /><small>Les spotlight sont des tournois avec dotation auxquels vous devez vous inscrire en avance. Ils se déroulent sur plusieurs heures le samedi après midi et en soirée, et le dimanche matin pour les finales.</small></h4>
+				<h4>Où se trouve le règlement des tournois spotlight ? <br /><small>Le règlement des spotlight sera disponible un mois avant la LAN.</small></h4>
+				<h4>Puis-je participer à plusieurs spotlight en même temps ?<br /><small>Hélas non, il ne vous est pas possible de participer à deux tournois en même temps. Nous en sommes sincèrement désolés.</small></h4>
+				<h4>Comment inscrire mon équipe à un spotlight ?<br /><small>La gestion des équipes sera disponible une fois les inscriptions ouvertes.</small></h4>
+				</div>
+		</a>
+		
+		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewteam">
+			<h3 class="list-group-item-heading">
+				<span class="glyphicon glyphicon-fire"></span> Les animations 
+			</h3>
+			<div class="list-group-item-text collapse" id="viewteam">
+				<h4>Que puis-je faire en dehors des tournois spotlight ?<br /><small>Des animations sont organisées du vendredi soir au dimanche après midi quasiment en continu.</small></h4>
+				<h4>Quels types d'animations sont organisées ?<br /><small>Des rencontre amicales avec ou sans lots sur tous les jeux de notre liste (voir la page des jeux), des speedruns, des jeux de société, des consoles, plein de stands, vous ne serez pas déçu.</small></h4>
+				<h4>Qu'est ce que vous avez en plus par rapport à une autre LAN ?<br /><small>Nous proposons pendant la LAN une application exclusive permettant aux joueurs de créer leurs tournois, visualiser les tournois à venir et en cours, participer à des animations, voir le ladder de la LAN et communiquer avec les autres joueurs et les admins.</small></h4>
+				<h4>Comment sont gérés les affrontements hors tournois ?<br /><small>A votre arrivée à l'UA il vous sera attribuée une couleur : rouge ou bleu. Nous appelerons de temps en temps des combatants des deux couleurs pour s'affronter sur des jeux hors spotlight. Vos victoires feront gagner des points et de la gloire à votre couleur.</small></h4>
+				<h4>Je dois forcément jouer pour ma couleur ? Et si je viens avec mes amis ?<br /><small>Nous vous encourageons à organiser tous ce qui vous plait, ARAM sur LOL, King of the Hill sur TF2, nous sommes ouverts à vos propositions et nous pourons fournir des goodies si vos idées nous intéressent.<small></h4>
+				<h4>Comment est-ce que je m'inscris aux animations ?<br /><small>Les animations seront visibles sur l'intranet, vous devrez vous connecter avec votre compte utilisateur et vous n'aurez qu'à cliquer pour vous inscrire. Les animations sont accessibles à tous les joueurs ayant payé leur entrée à l'événément.</small></h4>
+				<h4>Est ce que les visiteurs peuvent participer aux animations ?<br /><small>Un visiteur ne peut pas amener son PC, ni participer à une animation. En revanche les stands (dont des consoles) sont en accès libre.</small></h4>
 			</div>
 		</a>
 		
-		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewsubscribe">
+		<a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewcontacts">
 			<h3 class="list-group-item-heading">
-				<span class="glyphicon glyphicon-log-in"></span>	Inscription à l'UTT Arena 
+				<span class="glyphicon glyphicon-envelope"></span> Nous contacter 
 			</h3>
-			<div class="list-group-item-text collapse" id="viewsubscribe">
-				<h4>Comment je modifie mon profil ? <br /><small>Une fois connecté, vous disposez d'un bouton "Modifier le profil" en haut à droite (qui se retrouve aussi dans le sous-menu d'inscriptions).</small></h4>
+			<div class="list-group-item-text collapse" id="viewcontacts">
+				<h4>Je n'ai pas la réponse à ma question, qui dois-je contacter ?<br /><small>Vous pouvez contacter le staff de l'UA via ung@utt.fr. N'hésitez pas à vous exprimer en cas de doutes ou de remarques.</small></h4>
 			</div>
 		</a>
 		
