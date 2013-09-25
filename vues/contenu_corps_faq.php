03ab2c1d2f0f1f4484deb6d7ec94d5544e0e0951
Julien Nozais <julien.nozais@gmail.com>
2013-09-25 04:13:22 +0200

    Updated FAQ

--- vues/contenu_corps_faq.php
+++ vues/contenu_corps_faq.php
@@ -15,7 +15,7 @@
             </h3>
             <div class="list-group-item-text collapse" id="viewsubscribe">
                 <h4>Comment et quand payer ?<br /><small>Deux solutions s'offrent à vous : payer sur le site en prévente, dès l'ouverture des inscriptions, ou payer sur place, plein tarif (espèces uniquement). Les préventes commencent mi-octobre et se terminent 48 h avant l'UA. </small></h4>
-                <h4>Puis-je payer sur place ?<br /><small>Oui, mais une majoration sera appliquée, les prix passeront à 10 € pour les UTTiens, et à 15 € pour les extérieurs.</small></h4>
+                <h4>Puis-je payer sur place ?<br /><small>Oui, mais une majoration de 3 € sera appliquée, les prix passeront à 13 € pour les UTTiens, et à 18 € pour les extérieurs.</small></h4>
                 <h4>Puis-je me faire rembourser ? <br /><small>Nous traitons ce genre de demande au cas par cas, avec comme solution donner votre place à votre remplaçant ou trouver un moyen de vous dédommager. Pensez à contacter le staff UA par e-mail.</small></h4>
                 <h4>Les visiteurs sont-ils admis ?<br /><small>Les visiteurs doivent payer un droit d'entrée de 2 € à l'accueil, il leur sera remis un badge qu'ils doivent porter de façon visible. Les visiteurs ayant quitté la sale doivent présenter leur badge à l'accueil s'ils veulent rentrer.</small></h4>
 				<h4>Je suis encore mineur, est-ce que je peux venir ?<br /><small>Les mineurs de plus de 15 ans sont autorisés à condition de présenter une autorisation parentale à l'accueil de l'événement, sans quoi ils seront refusés.</small></h4>
@@ -30,7 +30,7 @@
                 <h4>A quelle heure commence la LAN ?<br /><small>L'UTT Arena ouvre ses portes le vendredi à 18 h. Les animations commencent à 19 h. Un planning des animations sera fourni avant la semaine de la LAN.</small></h4>
                 <h4>Où puis-je dormir ?<br /><small>Nous pouvons vous mettre à disposition une salle de repos commune de taille limitée, néanmoins nous ne pouvons vous fournir un véritable logement pour la nuit. Nous vous conseillons de trouver une solution, que ce soit de dormir dans un hôtel, dans vos voitures ou tout simplement de sommeiller devant votre écran.</small></h4>
                 <h4>Où manger au cours de l'événement ?<br /><small>La commission bouffe (dite aussi commission mangeaille) installera un stand près de l'accueil, avec une équipe chaleureuse et serviable à votre disposition (plus de 20h par jour) pour vous servir en boissons, barres, sandwiches, crêpes et nourritures diverses. </small></h4>
-                <h4>Quel matériel est fourni/loué ?<br /><small>A part cas exceptionnel, rien n'est fourni. Vous disposerez d'une prise par personne et d'un port réseau. Nous pouvons louer des écrans cathodiques et vendons des câbles réseaux Ethernet de 5 mètres.</small></h4>
+                <h4>Quel matériel est fourni/loué ?<br /><small>A part cas exceptionnel, rien n'est fourni. Vous disposerez d'une prise électrique par personne (pensez à prendre une multiprise) et d'un port réseau. Nous pouvons louer des écrans cathodiques et vendons des câbles réseaux Ethernet de 5 mètres.</small></h4>
                 <h4>Que faire lorsque j'arrive sur place ? <br /><small>Si vous arrivez par train et avez demandé une navette pour vous rendre à la LAN (demande via le forum), nous vous attendrons sur le quai (un numéro de téléphone vous sera fourni pour nous contacter en cas de soucis). Si vous arrivez sur le lieu de l'UA même (halle sportive de l'UTT), rendez vous à l'accueil où vous serez accueillis et obtiendrez votre badge.</small></h4>
             </div>
         </a>
@@ -41,10 +41,12 @@
             </h3>
             <div class="list-group-item-text collapse" id="viewtournaments">
                 <h4>Qu'est ce qu'un tournois spotlight ?<br /><small>Les spotlight sont des tournois avec dotation auxquels vous devez vous inscrire en avance. Ils se déroulent sur plusieurs heures le samedi après midi et en soirée, et le dimanche matin pour les finales.</small></h4>
-                <h4>Où se trouve le règlement des tournois spotlight ? <br /><small>Le règlement des spotlight sera disponible un mois avant la LAN.</small></h4>
+                <h4>Quand puis-je m'y inscrire ?<br /><small>Dès l'ouverture des inscriptions, jusqu'à 48 h avant le début de la LAN sur internet. Les inscriptions seront définitivement closes vendredi soir. Nous pourrons vous accueillir samedi si vous avez payé votre place, mais il sera trop tard pour vous inscrire à un spotlight.</small></h4>
+				<h4>Où se trouve le règlement des tournois spotlight ? <br /><small>Le règlement des spotlight sera disponible un mois avant la LAN.</small></h4>
                 <h4>Puis-je participer à plusieurs spotlight en même temps ?<br /><small>Hélas non, il ne vous est pas possible de participer à deux tournois en même temps. Nous en sommes sincèrement désolés.</small></h4>
                 <h4>Comment inscrire mon équipe à un spotlight ?<br /><small>La gestion des équipes sera disponible une fois les inscriptions ouvertes.</small></h4>
-                </div>
+                <h4>Que faire si mon jeu préféré n'est pas un spotlight ?<br /><small>A partir de cette année nous voulons mettre l'accent sur les activités hors tournois. Vous pouvez passez une excellente UTT Arena grâce à toutes les animations disponibles.</small></h4>
+				</div>
         </a>
 
         <a href="#" class="list-group-item collapse-group" data-toggle="collapse" data-target="#viewteam">
@@ -53,12 +55,12 @@
             </h3>
             <div class="list-group-item-text collapse" id="viewteam">
                 <h4>Que puis-je faire en dehors des tournois spotlight ?<br /><small>Des animations sont organisées du vendredi soir au dimanche après midi quasiment en continu.</small></h4>
-                <h4>Quels types d'animations sont organisées ?<br /><small>Des rencontre amicales avec ou sans lots sur tous les jeux de notre liste (voir la page des jeux), des speedruns, des jeux de société, des consoles, plein de stands, vous ne serez pas déçu.</small></h4>
-                <h4>Qu'est ce que vous avez en plus par rapport à une autre LAN ?<br /><small>Nous proposons pendant la LAN une application exclusive permettant aux joueurs de créer leurs tournois, visualiser les tournois à venir et en cours, participer à des animations, voir le ladder de la LAN et communiquer avec les autres joueurs et les admins.</small></h4>
+                <h4>Quels types d'animations sont organisées ?<br /><small>Des rencontre amicales avec ou sans lots sur tous les jeux de notre liste (voir la page des jeux), des speedruns, des jeux de société, des consoles, plein de stands avec nos partenaires, vous ne serez certainement pas déçu si vous venez pour ces activités.</small></h4>
+                <h4>Qu'est ce que vous avez en plus par rapport à une autre LAN ?<br /><small>Nous mettons à disposition des joueurs une application web exclusive permettant aux joueurs de créer leurs tournois, visualiser les tournois à venir et en cours, participer à des animations, voir le classement de la LAN et communiquer entre joueurs et avec les admins.</small></h4>
                 <h4>Comment sont gérés les affrontements hors tournois ?<br /><small>A votre arrivée à l'UA il vous sera attribuée une couleur : rouge ou bleu. Nous appelerons de temps en temps des combatants des deux couleurs pour s'affronter sur des jeux hors spotlight. Vos victoires feront gagner des points et de la gloire à votre couleur.</small></h4>
-                <h4>Je dois forcément jouer pour ma couleur ? Et si je viens avec mes amis ?<br /><small>Nous vous encourageons à organiser tous ce qui vous plait, ARAM sur LOL, King of the Hill sur TF2, nous sommes ouverts à vos propositions et nous pourons fournir des goodies si vos idées nous intéressent.<small></h4>
+                <h4>Je dois forcément jouer pour ma couleur ? Et si je viens avec mes amis ?<br /><small>Tout ne sera pas axé autour du rouge/bleu, de plus nous vous encourageons à vous-même organiser tout ce qui vous plait. ARAM sur LOL, King of the Hill sur TF2, nous sommes ouverts à vos propositions et nous pourons fournir des goodies si vos idées nous intéressent.<small></h4>
                 <h4>Comment est-ce que je m'inscris aux animations ?<br /><small>Les animations seront visibles sur l'intranet, vous devrez vous connecter avec votre compte utilisateur et vous n'aurez qu'à cliquer pour vous inscrire. Les animations sont accessibles à tous les joueurs ayant payé leur entrée à l'événément.</small></h4>
-                <h4>Est ce que les visiteurs peuvent participer aux animations ?<br /><small>Un visiteur ne peut pas amener son PC, ni participer à une animation. En revanche les stands (dont des consoles) sont en accès libre.</small></h4>
+                <h4>Est ce que les visiteurs peuvent participer aux animations ?<br /><small>Un visiteur ne peut pas amener son PC, ni participer à une animation. En revanche certains stands (dont des consoles de jeu) sont en accès libre aux visiteurs.</small></h4>
             </div>
         </a>
 
