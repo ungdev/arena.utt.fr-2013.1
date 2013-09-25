fd0287fee8b8a9d7088b8ba2f2c23dcefecd3535
uZer <piolet.y@gmail.com>
2013-09-25 05:40:42 +0200

    Better Description CSS
    TODO: Fix mobile version

--- vues/contenu_corps_description.php
+++ vues/contenu_corps_description.php
@@ -8,45 +8,50 @@
 
     </div>
 
+	<h2 id="nav-tabs">Déroulement de la LAN</h2>
+	<p>
+		<strong>L'UTT Arena change de formule !</strong>
+		Nous vous proposons de vous amuser lors d'animations présentes tout le week-end.
+		Découvrez le détail ci-dessous.
+	</p>
+	
     <div class="row">
 
             <div class="col-md-8">
 
-        <h2 id="nav-tabs">Déroulement de la LAN</h2>
-        <p>
-            L'UTT Arena change de formule !
-            Nous vous proposons de vous amuser lors d'animations présentes tout le week-end. Découvrez le détail ci-dessous.
-        </p>
             <div class="panel panel-default">
-            <div class="bs-example panel-body">
-                <ul class="nav nav-tabs" style="margin-bottom: 15px;">
+            <div class="panel-header" style="padding-top: 10px;">
+                <ul class="nav nav-tabs">
                     <li class="active"><a href="#libre" data-toggle="tab">Tournois libres</a></li>
-                    <li class=""><a href="#spotlight" data-toggle="tab">Tournois spotlight</a></li>
+                    <li class=""><a href="#spotlight" data-toggle="tab">Spotlight</a></li>
                     <li class=""><a href="#animations" data-toggle="tab">Animations</a></li>
                     <li class=""><a href="#plateau" data-toggle="tab">Jeux sur table</a></li>
-                    <li class=""><a href="#invites" data-toggle="tab">Invités et VIP</a></li>
+                    <li class=""><a href="#invites" data-toggle="tab">Invités</a></li>
+                    <li class=""><a href="#autres" data-toggle="tab">Autres...</a></li>
                 </ul>
-
+			</div>
+			
+            <div class="panel-body">
                 <div id="myTabContent" class="tab-content">
                     <div class="tab-pane fade active in" id="libre">
                         <h3>Les tournois libres</h3>
                         <p>Grâce à notre site intranet, vous serez informés en live. L'équipe vous proposera des jeux et des petits tournois toute la journée : défis sur notre serveur Minecraft, tournois sur console, challenge speedrun sur Portal et bien d'autres.
-        Vous pourrez ainsi remporter des lots !
-        <br/>
-        Et si vous voulez faire des matchs avec vos voisins ou un inconnu à l'autre bout de la salle, l'intranet vous permettra de trouver vos adversaires et d'organiser vos rencontres. N'oubliez pas d'aller regarder la liste des jeux !</p>
+        Vous pourrez ainsi remporter des lots !</p>
+   
+        <p>Et si vous voulez faire des matchs avec vos voisins ou un inconnu à l'autre bout de la salle, l'intranet vous permettra de trouver vos adversaires et d'organiser vos rencontres. N'oubliez pas d'aller regarder la liste des jeux !</p>
 
                     </div>
 
                     <div class="tab-pane fade" id="spotlight">
-                        <h3>Les tournois Spotline</h3>
+                        <h3>Les tournois Spotlight</h3>
                         <p>Vous êtes fan de League of Legends ou de Starcraft II ? Nous vous organisons le samedi après-midi et le samedi soir deux sessions de tournois Spotlight. Durant deux sessions de 3h, venez combattre en solo ou avec votre équipe et remportez des lots !</p>
                     </div>
 
                     <div class="tab-pane fade" id="animations">
                         <h3>Les animations des partenaires</h3>
-                        <p>Chaque année, nous avons le plaisir de recevoir Macommune.org et SaveGames. <br/>
-        Macommune.org est une communauté qui vous fait découvrir des <a href="http://fr.wikipedia.org/wiki/Libre_de_droits">jeux libres de droits</a>. Prenez place avec vos amis devant l'un des 16 PCs en libre accès dans leur stand, et amusez vous sur TeeWorlds, UrbanTerror, ArmageTron et bien d'autres pépites. <br/>
-        SaveGames est un magasin de jeux vidéos situé au centre-ville de Troyes. Ils nous proposent des consoles récentes en libre accès, avec de nombreux jeux récents. <br/></p>
+                        <p>Chaque année, nous avons le plaisir de recevoir Macommune.org et SaveGames.</p>
+        <p>Macommune.org est une communauté qui vous fait découvrir des <a href="http://fr.wikipedia.org/wiki/Libre_de_droits">jeux libres de droits</a>. Prenez place avec vos amis devant l'un des 16 PCs en libre accès dans leur stand, et amusez vous sur TeeWorlds, UrbanTerror, ArmageTron et bien d'autres pépites.</p>
+        <p>SaveGames est un magasin de jeux vidéos situé au centre-ville de Troyes. Ils nous proposent des consoles récentes en libre accès, avec de nombreux jeux récents.</p>
                     </div>
 
                     <div class="tab-pane fade" id="plateau">
@@ -61,13 +66,18 @@
                         <p>Sur la scène, vous allez retrouver plein d'animations, et surtout des matchs en streaming. Pour les commenter, nous accueillons des invités que vous connaissez peut être... Mais chut, pour le moment c'est une surprise !
                         </p>
                     </div>
+
+                    <div class="tab-pane fade" id="autres">
+						<h3>Et encore plein d'autres choses...</h3>
+						<p>Nous avons encore des tas de projets dans les cartons et des surprises à découvrir pendant l'événement, alors n'hésitez pas à vous tenir informés sur les réseaux sociaux et la newsletter.</p>
+						</div>
                </div>
             </div>
             </div>
 
 
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4"><div class="panel panel-default"><div class="panel-body">
             <div id="myCarousel" class="carousel slide" style="max-width: 720px;">
                 <ol class="carousel-indicators">
                   <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -118,11 +128,11 @@
                 <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
               </div>
             </div>
+            </div>
+            </div>
 
     </div>
 
-        <h3>Et encore plein d'autres choses...</h3>
-        Nous avons encore des tas de projets dans les cartons et des surprises à découvrir pendant l'événement, alors n'hésitez pas à vous tenir informés sur les réseaux sociaux et la newsletter.
 
         <h3>Note pour les mineurs</h3>
         Les mineurs de plus de 15 ans sont autorisés à condition de présenter une <a href="pdf/autorisation_parentale.pdf">autorisation parentale</a> à l'accueil de l'événement, sans quoi ils seront refusés.
